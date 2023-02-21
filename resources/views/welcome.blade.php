d8e261c566958edaab5a2fcb9b0394d977903bcb
Kate Rochelle Onido <katonido@my.cspc.edu.ph>
2023-02-21 11:08:48 +0800

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+!\Doctype <html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equi="UA-Compatible" content="Ie=-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.">
+    <title>Onido</title>
+<head>
+<body>
+    <h1>My very First Laravel Application</h1>
+    <h2>Welcome to my First Laravel Application</h2>
+</body>
+</html>
+
